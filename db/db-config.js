2b69d0dc0bd2cfd59ee4503e0f7f4496626d5bdd
bekure27 <bekure27@gmail.com>
2023-10-21 17:23:06 +0300

    update db-config

--- db/db-config.js
+++ db/db-config.js
@@ -1,16 +1,22 @@
 const mysq12 = require("mysql2");
 
-const dbconn = mysq12.createPool({
+const dbcon = mysq12.createPool({
   user: "forum_admin",
   database: "forum_db",
   host: "localhost",
   password: "12345678",
   connectionLimit: 10,
 });
-dbconn.execute("select 'test' ", (err, result) => {
-  if (err) {
-    console.log(err.message);
-  } else {
-    console.log(result);
-  }
-});
+
+// using callback 
+
+// dbcon.execute("select 'test' ", (err, result) => {
+//   if (err) {
+//     console.log(err.message);
+//   } else {
+//     console.log(result);
+//   }
+// });
+
+
+module.exports = dbcon.promise()

795153b57c62e6e17b4481d8e492452226188eaf
bekure27 <bekure27@gmail.com>
2023-10-30 18:55:16 +0300

    update style

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,15 +3,15 @@ import LoginRegister from "../components/LoginRegister/LoginRegister"
 
 const LandingPage = () => {
   return (
-    <div
-      className=" bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url('./10001.svg')` }}
-    >
-      <div className=" py-28 px-28 sm:flex ">
-        <LoginRegister />
-        <About />
+      <div
+        className=" bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url('./10001.svg')` }}
+      >
+        <div className=" py-28 px-28 sm:flex ">
+          <LoginRegister />
+          <About />
+        </div>
       </div>
-    </div>
   );
 }
 

57bc958f09f316e5c7a182718f4ab9e52e5a222b
bekure27 <bekure27@gmail.com>
2023-11-06 09:06:40 +0300

    update 404 style

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
     <div className="flex h-screen bg-gray-200">
       <div className="m-auto text-center">
         <h1 className="text-6xl font-bold">404</h1>
-        <p className="text-2xl mt-4">Page Not Found</p>
+        <p className="text-4xl mt-4">Page Not Found</p>
       </div>
     </div>
   );
